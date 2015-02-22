813a1314ae8264c3ade395935b5129b7779a28ac
Jussi Heikkinen <jussih91@gmail.com>
2015-02-22 20:26:07 +0200

    Delete db-init.php

--- db-init.php
+++
@@ -1,7 +0,0 @@
-<?php
-// db-init.php
-$db = new PDO('mysql:host=192.168.10.53;dbname=omaDB;charset=utf8',
-              'jussi', 'kalamies');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-?>
