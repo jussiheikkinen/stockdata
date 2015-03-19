4f4fb970d612a3717dbd0d916aeee398358494d1
jussiheikkinen <jussih91@gmail.com>
2015-03-19 18:58:27 +0200

    uutta

--- user.php
+++ user.php
@@ -62,18 +62,6 @@ myyOsake($val);
 <?php
 $oletusSalkku = new Salkku();
 $oletusSalkku->tulostaSalkku($_SESSION['userName']);
-/*
-//UUDEN OSAKKEEN LiSÄYS
-include ('newstock.php');
-
-if(isset($_GET['addstock'])){
-lisaaOsake($oletusSalkku->salkkuID);
-}
-
-if(isset($_GET['sellstock'])){
-$val = $_GET['stock1'];
-myyOsake($val);
-}*/
 ?>
 </article>
 
