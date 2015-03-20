93464789e21735d9fdeae44ffc1ab9ef016d82ac
jussiheikkinen <jussih91@gmail.com>
2015-03-20 23:03:12 +0200

    feed

--- feed.php
+++ feed.php
@@ -45,13 +45,11 @@ echo '<table id="omasalkku"><tr><th>time</th><th>name</th><th>poo</th><th>poo</t
 echo <<<SALKKU
      <tr><td>$osumat[0][0]</td><td>$osumat[$i][1]</td><td>$osumat[$i][5]</td>
      <td>$osumat[$i][11]</td><td>$osumat[$i][12]</tr>
-     </table>;
 SALKKU;
 $i++;
    }
    echo "</table>";
    //print_r ($matches);
-
    }
 
 
