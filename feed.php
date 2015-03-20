e62d713eb57bd45edf4849eb53250608b7f3bba1
jussiheikkinen <jussih91@gmail.com>
2015-03-20 11:33:24 +0200

    feed

--- feed.php
+++ feed.php
@@ -8,7 +8,6 @@ exit;}
 
 <html style="width:100%;" >
 <head>
-<!--<meta http-equiv="refresh" content="15" >-->
 <title>Stocksdata</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="icon.ico">
@@ -22,7 +21,22 @@ exit;}
 <header>
 <?php include ('header.php'); ?>
 </header>
+<content>
+<article id = "kayttajat">
 
+  <?php
+
+  $homepage = file_get_contents("https://www.nordnet.fi/mux/web/marknaden/kurslista/aktier.html");
+
+     if (preg_match_all('/class=.underline.>[A-Za-z\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
+       echo "loytyi";
+       print_r ($matches);
+     }
+  ?>
+
+<article>
+
+</content>
 </body>
 
 </html>
