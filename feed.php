b861aea54910e08a2da11fcb2c47a33ef99c2015
jussiheikkinen <jussih91@gmail.com>
2015-03-20 22:23:27 +0200

    feed

--- feed.php
+++ feed.php
@@ -28,35 +28,24 @@ exit;}
 
 <?php
 
-$homepage = file_get_contents("https://www.nordnet.fi/mux/web/marknaden/kurslista/aktier.html");
+$homepage = file_get_contents("https://www.nordnet.fi/mux/web/marknaden/kurslista/aktier.html?marknad=Finland&lista=1_1&large=on&mid=on&small=on&sektor=0&subtyp=price&sortera=aktie&sorteringsordning=stigande");
 
    //if (preg_match_all('/underline.>[A-Za-z\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
-if (preg_match_all('/[A-Z]+[A-Za-z\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
+   if (preg_match_all('/[A-Z]+[A-Za-z\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
 
-     	$i = 0;
-	$j = count($matches, COUNT_RECURSIVE) -1 ;
-	while ($i < $j){
+        	$i = 0;
+   	$j = count($matches, COUNT_RECURSIVE) -1 ;
+   	while ($i < $j){
 
-	$data = preg_match_all('/[A-Z]+[A-Za-z\s]+|[0-9,%]+/', $matches[0][$i], $osumat);
+   	preg_match_all('/[A-Z]+[A-Za-z\s]+|[0-9,%]+/', $matches[0][$i], $osumat);
 
-	$i++;
-print_r ($osumat);
-}
+   	$i++;
+   }
 
-print_r ($matches);
+   print_r ($matches);
 
-}
+   }
 
-/*	$i = 0;
-	$j = count($matches, COUNT_RECURSIVE) -1 ;
-	while ($i < $j){
-	$data = explode("</td>", $matches[0][$i]);// näyttää tältä <td  >37,13</td>
-						// näyttää tältä <td  >37,13
-	$taulu[] = implode("<td >",$data); 	// näyttää tältä <td  >37,13<td >
-	$taulu1[] = explode("<td >",$taulu[$i]);	// pitäsi näyttää tältä 7,13, mutta näytää tältä <td  >37,13
-	$i++;	}   [A-Z]|([a-z]){4,30}\w+|[0-9,%]
-			/<td.class[a-z&0-9=\?.\/"\s<>]+[A-Za-z\s<\/>0-9,="%]+EUR<\/td>/
-     */
 ?>
 
 </pre>
