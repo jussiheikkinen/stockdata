c02522555baca6af2d36518a622fefea42e24ca7
jussiheikkinen <jussih91@gmail..com>
2015-03-24 19:55:13 +0200

    korjauksia

--- feed.php
+++ feed.php
@@ -30,8 +30,8 @@ exit;}
 $homepage = file_get_contents("https://www.nordnet.fi/mux/web/marknaden/kurslista/aktier.html?marknad=Finland&lista=1_1&large=on&mid=on&small=on&sektor=0&subtyp=price&sortera=aktie&sorteringsordning=stigande");
 
 echo '<table id="omasalkku"><tr><th>Name</th><th>Last</th><th>Change</th><th>%</th><th>Highest</th><th>Lowest</th><th>Volume</th><th>Currency</th></tr>';
-   //if (preg_match_all('/underline.>[A-Za-z\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
-   if (preg_match_all('/[A-Z]+[A-Za-z\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
+   //if (preg_match_all('/underline.>[A-Za-ö\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
+   if (preg_match_all('/[A-Z]+[A-Za-ö\s<\/>0-9,="%]+EUR/',$homepage , $matches)){
 
         	$i = 0;
    	$j = count($matches, COUNT_RECURSIVE) -1 ;
