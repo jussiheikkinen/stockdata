b4c720550aed8e212086aa9f1b05bb22edd7beea
jussiheikkinen <jussih91@gmail..com>
2015-03-24 21:19:52 +0200

    korjauksia

--- feed.php
+++ feed.php
@@ -53,11 +53,11 @@ echo '<table id="omasalkku"><tr><th>Name</th><th>Last</th><th>Change</th><th>%</
    }
 
    if ($osumat[0][2] == Plus){
-     $color = "color:red";
+     $color = '"color:red;"';
    }
 
 echo <<<SALKKU
-     <tr><td>{$osumat[0][0]}</td><td>{$osumat[0][1]}</td><td>{$osumat[0][3]}</td><td style="<?php echo $color;?>">{$osumat[0][5]}</td>
+     <tr><td>{$osumat[0][0]}</td><td>{$osumat[0][1]}</td><td>{$osumat[0][3]}</td><td style=$color>{$osumat[0][5]}</td>
      <td>{$osumat[0][8]}</td><td>{$osumat[0][9]}</td><td>$volume</td><td>$valuutta</td></tr>
 SALKKU;
 $i++;
