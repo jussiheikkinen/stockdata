8d3b3496efe391ddc7eaa68a0cd946f556154a71
jussiheikkinen <jussih91@gmail..com>
2015-04-09 15:36:49 +0300

    kotimaisia

--- feed.php
+++ feed.php
@@ -90,7 +90,7 @@ NEW;
 require_once 'Salkku.class.php';
 require ("/var/www/db-init.php");
 
-if(isset($_REQUEST['amount'])){
+if(isset($_GET["amount"])){
 $a = $oletusSalkku->salkkuID;
 $b =  $_SESSION['userName'];
 $lkm = $_GET['amount'];
