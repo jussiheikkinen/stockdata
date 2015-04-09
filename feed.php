107bac06445a8877cc7b9acfa91d0589d9de57e4
jussiheikkinen <jussih91@gmail..com>
2015-04-09 14:27:14 +0300

    feed/form

--- feed.php
+++ feed.php
@@ -71,7 +71,6 @@ echo "</table>";
 <article>
 <article id="kayttaja" style="float:right; margin-left:10%;">
   <?php
-  include (newstock);
   $hinta = (double)$_GET[hinta];
 
   echo <<<NEW
@@ -89,7 +88,37 @@ NEW;
 
 require_once 'Salkku.class.php';
 $oletusSalkku = new Salkku();
-lisaaKotimainenOsake($oletusSalkku->salkkuID, $hinta, $_GET[amount] );
+
+require ("/var/www/db-init.php");
+
+$a = $oletusSalkku->salkkuID;
+$b =  $_SESSION['userName'];
+$tunnus = $_GET[osake]; //Osakkeet aina isoilla kirjaimilla
+$ostohinta = $hinta;
+$lkm = $_GET['amount'];
+
+$stmt = $db->prepare("SELECT SalkkuId FROM Salkku INNER JOIN Kayttaja ON KayttajaId = SalkkuKayttaja WHERE KayttajaNimi =?");
+$stmt->execute(array($b));
+$salkkuid = $stmt->fetch(PDO::FETCH_OBJ);
+
+$stmt = $db->prepare("SELECT TiedotId FROM Tiedot WHERE TiedotId =?");
+$stmt->execute(array(1));
+$tiedotid =  $stmt->fetch(PDO::FETCH_OBJ);
+
+$stmt = $db->prepare("INSERT INTO Osake (OsakeNimi, OsakeTiedot) VALUES (?, ?)");
+$stmt->execute(array($tunnus, $tiedotid->TiedotId));
+
+$stmt = $db->prepare("SELECT OsakeId FROM Osake WHERE OsakeNimi =?");
+$stmt->execute(array($tunnus));
+$osakeid =  $stmt->fetch(PDO::FETCH_OBJ);
+
+$stmt = $db->prepare("INSERT INTO Tapahtuma (TapahtumaLkm, TapahtumaHinta, TapahtumaSalkku, TapahtumaOsake) VALUES( :f1,:f2,:f3,:f4)");
+$stmt->execute(array(':f1' => $lkm, ':f2' => $ostohinta, ':f3' => $salkkuid->SalkkuId, ':f4' => $osakeid->OsakeId));
+if ($affected_rows = $stmt->rowCount()){
+   echo '<META HTTP-EQUIV="Refresh" Content="0; URL=user.php">';
+} else {
+exit();
+}
 ?>
 
 <article>
