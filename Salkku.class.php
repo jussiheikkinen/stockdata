6cec2b715757844c9c68185ed958b44b883d135b
jussiheikkinen <jussih91@gmail..com>
2015-04-09 18:55:15 +0300

    18.10

--- Salkku.class.php
+++ Salkku.class.php
@@ -62,7 +62,7 @@ $arvo = $hinta + (($kurssi[0] - $row['TapahtumaHinta']) * $row['TapahtumaLkm']);
 $prosentti = round((($arvo/$hinta)*100)-100, 1, PHP_ROUND_HALF_UP);//pyöristys
 
 echo <<<SALKKU
-<tr><td>{$row['TapahtumaAika']></td><td><a href="user.php?osake={$row['OsakeNimi']}">{$row['OsakeNimi']}</a></td><td>{$row['TapahtumaHinta']}</td>
+<tr><td>{$row['TapahtumaAika']></td><td><a href="user.php?osake=$row['OsakeNimi']">{$row['OsakeNimi']}</a></td><td>{$row['TapahtumaHinta']}</td>
 <td>{$row['TapahtumaLkm']}</td><td>$kurssi[0]</td><td>$arvo</td><td>$prosentti</td><td>{$row['TiedotValuutta']}</td></tr>
 SALKKU;
 }
