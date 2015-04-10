f24917abdeedda7fa9193ffd286cdd8c09611111
jussiheikkinen <jussih91@gmail..com>
2015-04-10 10:48:21 +0300

    price NULL

--- user.php
+++ user.php
@@ -14,6 +14,25 @@ require_once 'Osake.class.php';
 <link rel="shortcut icon" href="icon.ico">
 <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="tyylit.css">
+
+<script>
+function showHint(str) {
+    if (str.length == 0) {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET", "gethint.php?q=" + str, true);
+        xmlhttp.send();
+    }
+}
+</script>
+
 </head>
 
 <body>
